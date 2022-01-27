48f9e4a7278eda25427f17e1d98db86037444ebe
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-01-27 12:38:33 +0000

    task 8 list cities

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Must be listening on 0.0.0.0, port 5000
+Must use storage for fetching data from the storage engine
+Remove the current SQLAlchemy Session after each request
+"""
+
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.routes("/states_list", strict_slashes=False)
+def list_states():
+    """
+    Renders states in HTML template
+    """
+    states = storage.all('State')
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def remove_session(context):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
